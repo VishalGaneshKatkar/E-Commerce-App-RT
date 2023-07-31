4eb1dad067b9f4e545e09c34aca9584baaee9a4f
VishalGaneshKatkar <vgkatkar20@gmail.com>
2023-07-31 19:06:47 +0530

    resolved merge conflicts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,11 +14,7 @@ const Navbar = () => {
                 justifyContent: 'space-between',
             }}
         >
-<<<<<<< HEAD
             <Link to='/' className="logo"><h2>Shopping Cart App</h2></Link>
-=======
-            <Link to='/' className="logo"><h2>E Commerce App</h2></Link>
->>>>>>> c0b3def6c27a19c820dc40e464fd118579bb6e85
             <div className='links'>
                 <Link className="navLink" to="/">
                     Home
